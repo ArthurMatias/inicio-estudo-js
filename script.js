0d10a95235f598066da96f414d851e6b3c2f7677
arthur <arthurmatias2002@hotmail.com>
2022-08-14 01:32:17 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,18 @@
+//AULA 06 CURSO EM VIDEO JS
+
+//var nome  = window.prompt ('qual o seu nome?') //pergunta o nome no quadradinho do navegador
+//window.alert ('bem vindo ' + nome + '!')  // o + é um simbolo de concatenação
+
+//var n1 = Number(window.prompt ('digite um numero'))
+//var n2 = Number(window.prompt ('digite outro numero'))
+//var s = n1 + n2
+
+//window.alert ('seu resultado é ' + s)  //metodo funcional porem mais antigo e menos simplificado
+
+//window.alert (`a soma entre ${n2} e ${n2} é igual a ${s}`) //metodo mais simples de compreender, compativel e atual usando o ${}
+
+var nome  = window.prompt ('qual o seu nome?')
+
+var idade = window.prompt ('qual sua idade?')
+
+document.write (`Olá ${nome}, seja bem vindo!`) //aparece no body do html oque foi gravado pelo var selecionado dentro do ()
